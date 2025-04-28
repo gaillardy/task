4b995afb8e2ba084e1008bfbce889ccf56230689
Yohan <geektateur263@gmail.com>
2025-04-28 21:34:50 +0300

    Configuration de VIte

---
+++ resources/views/base.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/search.js'])
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <title>@yield('title') / Gestion de tâche</title>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+@extends('base')
+@section('title','Accueil')
+@section('content')
+@endsection
