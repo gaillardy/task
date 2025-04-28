3a9f76d0aa55689a79467f5d4b0117017a0af048
Yohan <geektateur263@gmail.com>
2025-04-28 22:34:19 +0300

    Integration : création de la page d'accueil.

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/search.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <title>@yield('title') / Gestion de tâche</title>
 </head>
 <body>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,25 @@
 @extends('base')
 @section('title','Accueil')
 @section('content')
+<nav>
+    <div class="nav-title"><a href="/">Gestion de Tâche</a></div>
+    <a href="https://github.com/gaillardy/task.git" target="_blank" rel="noopener noreferrer" class="github-logo">
+        <i class="fab fa-github"></i>
+    </a>
+</nav>
+
+<main class="main-content">
+    <h1 class="welcome-title">Bienvenue sur notre API de Gestion de Tâches</h1>
+    <p class="welcome-message">
+        Une solution puissante pour organiser vos projets et suivre vos tâches efficacement.
+        Commencez dès maintenant à optimiser votre productivité.
+    </p>
+    
+    <div class="buttons-container">
+        <a href="#" class="btn btn-primary">Voir les tâches</a>
+        <a href="https://github.com/gaillardy/task.git" target="_blank" rel="noopener noreferrer" class="btn btn-secondary">
+            <i class="fas fa-star"></i> Étoiles Github
+        </a>
+    </div>
+</main>
 @endsection
