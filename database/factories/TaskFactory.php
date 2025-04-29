e800559574878cc717ea478f265906ba36bd4de0
yohana Dev <yohana@mkweblinks.com>
2025-04-29 14:30:51 +0200

    Mise en place des Factory et Seeders

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(6),
+            'completed' => $this->faker->boolean(20),
+            'description' => $this->faker->optional(0.5)->paragraph(3),
+        ];
+    }
+}
